62ca6cb0ee4b4c0208fc7b7d8676a52fbe74751e
Fiona-Nyadero <fionanyadero@gmail.com>
2024-05-09 17:51:55 +0000

    Task0 corr

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,18 +6,18 @@ def validUTF8(data):
     '''Returns True if UTF-8 encoding is valid'''
     bytes_checker = 0
 
-    for num in data:
+    for n in data:
         if bytes_checker == 0:
-            if (num >> 3) == 0b11110:
+            if (n >> 3) == 0b11110:
                 bytes_checker = 3
-            elif (num >> 4) == 0b1110:
+            elif (n >> 4) == 0b1110:
                 bytes_checker = 2
-            elif (num >> 5) == 0b110:
+            elif (n >> 5) == 0b110:
                 bytes_checker = 1
-            elif (num >> 7):
+            elif (n >> 7):
                 return False
         else:
-            if (num >> 6) != 0b10:
+            if (n >> 6) != 0b10:
                 return False
             bytes_checker -= 1
 
