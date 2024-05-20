b296bde00601dee42afe1bfd32111ec4ca01eeb8
Fiona-Nyadero <fionanyadero@gmail.com>
2024-05-20 02:46:29 +0000

    Task 9 Corr- made file executable

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,58 +1,57 @@
 #!/usr/bin/python3
-'''mOdule implements the Nqueens game'''
+'''Module for the Nqueens game'''
 import sys
 
 
-def solve_queens_problem(board_size):
-    '''Returns n number of queens based on boardsize'''
+def is_safe(board, row, col):
+    '''Checks if position on board is safe'''
+    for f in range(row):
+        if board[f] == col or \
+           board[f] - f == col - row or \
+           board[f] + f == col + row:
+            return False
+    return True
 
-    def is_valid_position(position, occupied_pos):
-        '''Checks if a pos is occupied'''
-        for f in range(len(occupied)):
-            if (
-                occupied[f] == position or
-                occupied[f] - f == position - len(occupied) or
-                occupiedf] + f == position + len(occupied)
-            ):
-                return False
-        return True
 
-    def place_queens(board_size, i, occupied, sol):
-        '''Places Queens on the board'''
-        if i == board_size:
-            sol.append(occupied[:])
-            return
+def solve_nqueens(board, row, n):
+    '''Places n no. of Queens on board'''
+    if row == n:
+        print_board(board, n)
+        return
 
-        for f in range(board_size):
-            if is_valid_position(f, occupied):
-                occupied.append(f)
-                place_queens(board_size, i + 1, occupied, sol)
-                occupied.pop()
+    for col in range(n):
+        if is_safe(board, row, col):
+            board[row] = col
+            solve_nqueens(board, row + 1, n)
+            board[row] = -1  # Backtrack
 
-    sol = []
-    place_queens(board_size, 0, [], sol)
-    return sol
+
+def print_board(board, n):
+    '''Prints out the board with n Queens on stdout'''
+    solution = []
+    for i in range(n):
+        solution.append([i, board[i]])
+    print(solution)
 
 
 def main():
-    '''main fx'''
+    '''Entry point of the program'''
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
     try:
-        board_size = int(sys.argv[1])
+        n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
 
-    if board_size < 4:
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    sols = solve_queens_problem(board_size)
-    for sol in sols:
-        print([[i, sol[i]] for i in range(len(sol))])
+    board = [-1] * n
+    solve_nqueens(board, 0, n)
 
 
 if __name__ == "__main__":
