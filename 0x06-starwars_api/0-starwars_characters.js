e7e8855a180a9a5d01d666046cc1520ee8598146
Fiona-Nyadero <fionanyadero@gmail.com>
2024-05-23 17:28:50 +0000

    Task 0 - Star Wars Characters from API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
+
+  const characterPromises = characters.map(characterUrl => {
+    return new Promise((resolve, reject) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        const characterData = JSON.parse(body);
+        resolve(characterData.name);
+      });
+    });
+  });
+
+  Promise.all(characterPromises)
+    .then(names => {
+      names.forEach(name => console.log(name));
+    })
+    .catch(error => console.error(error));
+});
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,7 @@
+0x06. Star Wars API
+This is a test on:
+        -> HTTP Requests in JavaScript
+        -> Working with APIs
+        -> Asynchronous Programming
+        -> Command Line Arguments in Node.js
+        -> Array Manipulation and Iteration
