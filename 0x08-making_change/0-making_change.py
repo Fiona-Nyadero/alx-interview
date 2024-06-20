c33fb8ea2791e4df59a0587cf5a73aea56b631ad
Fiona-Nyadero <fionanyadero@gmail.com>
2024-06-20 20:16:54 +0000

    Task 0 - Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Module for making change fx'''
+
+def makeChange(coins, total):
+    '''Returns fewest no. of coins needed to meet total'''
+
+    if total < 1:
+        return 0
+    
+    coins.sort(reverse=True)
+
+    dp = 0
+
+    for coin in coins:
+        if total == 0:
+            break
+        amount = total // coin
+        total -= amount * coin
+        dp += amount
+
+    return dp if total == 0 else -1
