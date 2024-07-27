cabfa114742f6387e0afda73db458e4ccd2345c2
Fiona-Nyadero <125596787+Fiona-Nyadero@users.noreply.github.com>
2024-07-27 23:04:03 +0300

    Update 0-validate_utf8.py fixed

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,21 +4,26 @@
 
 def validUTF8(data):
     '''Returns True if UTF-8 encoding is valid'''
-    bytes_checker = 0
+    def is_start_byte(byte):
+        return (byte & 128) == 0 or (byte & 224) == 192 or \
+               (byte & 240) == 224 or (byte & 248) == 240
 
+    def is_continuation_byte(byte):
+        return (byte & 192) == 128
+
+    bytes_checker = 0
     for n in data:
         if bytes_checker == 0:
-            if (n >> 3) == 0b11110:
-                bytes_checker = 3
-            elif (n >> 4) == 0b1110:
-                bytes_checker = 2
-            elif (n >> 5) == 0b110:
-                bytes_checker = 1
-            elif (n >> 7) & 1:
+            if not is_start_byte(n):
                 return False
+            if (n & 224) == 192:
+                bytes_checker = 1
+            elif (n & 240) == 224:
+                bytes_checker = 2
+            elif (n & 248) == 240:
+                bytes_checker = 3
         else:
-            if (n >> 6) != 0b10:
+            if not is_continuation_byte(n):
                 return False
             bytes_checker -= 1
-
     return bytes_checker == 0
